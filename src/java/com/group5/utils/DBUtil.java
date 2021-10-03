7d7776a08b4007ab862dc4a411c6f22db35bc765
lmk-kuroshi <lekhoa305@gmail.com>
2021-10-03 16:08:16 +0700

    user sa pass 1

--- src/java/com/group5/utils/DBUtil.java
+++ src/java/com/group5/utils/DBUtil.java
@@ -18,7 +18,7 @@ public class DBUtil {
         Connection conn = null;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String url = "jdbc:sqlserver://localhost:1433;dataBaseName=EventManagement";
-        conn = DriverManager.getConnection(url, "prjtestuser", "123456");
+        conn = DriverManager.getConnection(url, "sa", "1");
         return conn;
     }
 }
