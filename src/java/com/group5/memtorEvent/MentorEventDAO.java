629a26e58a0f98c8407a0ef7f24b67b02616d0be
lmk-kuroshi <91231060+lmk-kuroshi@users.noreply.github.com>
2021-10-27 22:15:56 +0700

    add mentor event management

--- src/java/com/group5/memtorEvent/MentorEventDAO.java
+++ src/java/com/group5/memtorEvent/MentorEventDAO.java
@@ -18,20 +18,32 @@ import java.util.List;
  * @author Minh Khoa
  */
 public class MentorEventDAO {
-//    public List<MentorEventDTO> getListMentorInThisEvent(String eventID){
-//        List<MentorEventDTO> list = new ArrayList<>();
-//        Connection conn = null;
-//        PreparedStatement stm = null;
-//        ResultSet rs = null;
-//        try {
-//            conn=DBUtil.getConnection();
-//            if(conn!=null){
-//                String sql = "SELECT userID userName "
-//            }
-//        } catch (Exception e) {
-//        }
-//        return list;
-//    }
+    public List<MentorEventDTO> getListMentorInThisEvent(String eventID){
+        List<MentorEventDTO> list = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn=DBUtil.getConnection();
+            if(conn!=null){
+                String sql = "SELECT mentorEventID, mentorID, userName FROM tblMentorEvent, tblUser"
+                        + " WHERE mentorID = userID "
+                        + " AND eventID = ? "
+                        + " AND mentorEventStatus = 'ACT' ";
+                stm=conn.prepareStatement(sql);
+                stm.setString(1, eventID);
+                rs=stm.executeQuery();
+                while (rs.next()) {                    
+                    String mentorEventID = rs.getString("mentorEventID");
+                    String mentorID = rs.getString("mentorID");
+                    String mentorName = rs.getString("userName");
+                    list.add( new MentorEventDTO(mentorEventID, eventID, mentorID, mentorName, mentorEventID));
+                }
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
     
     public MentorEventDTO checkMentorEvent(String eventID, String mentorID) throws SQLException{
         MentorEventDTO mentorEvent = new MentorEventDTO();
@@ -41,14 +53,14 @@ public class MentorEventDAO {
         try{
             con=DBUtil.getConnection();
             if(con!=null){
-                String sql = "SELECT mentorEventID, mentorEventStatus FROM tblFollow WHERE eventID=? AND mentorID=?";
+                String sql = "SELECT mentorEventID, mentorEventStatus FROM tblMentorEvent WHERE eventID=? AND mentorID=?";
                 stm=con.prepareStatement(sql);
                 stm.setString(1, eventID);
                 stm.setString(2, mentorID);
                 rs=stm.executeQuery();
                 while (rs.next()) {                    
-                    String followID = rs.getString("followID");
-                    String followStatus = rs.getString("followStatus");
+                    String followID = rs.getString("mentorEventID");
+                    String followStatus = rs.getString("mentorEventStatus");
                     mentorEvent = new MentorEventDTO(followID, eventID, mentorID, followStatus);
                 }
             }
@@ -68,5 +80,81 @@ public class MentorEventDAO {
         return mentorEvent;
     }
     
+    public boolean addMentorEvent (MentorEventDTO mentorEvent) throws ClassNotFoundException, SQLException{
+        boolean check=false;
+        Connection con = null;
+        PreparedStatement stm = null;
+        try{
+            con=DBUtil.getConnection();
+            if(con !=null){
+                String sql = "INSERT INTO tblMentorEvent(mentorEventID, eventID, mentorID, mentorEventStatus) VALUES (?,?,?,?) ";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, mentorEvent.getMentorEventID());
+                stm.setString(2, mentorEvent.getEventID());
+                stm.setString(3, mentorEvent.getMentorID());
+                stm.setString(4, mentorEvent.getMentorEventStatus());
+                check = stm.executeUpdate() > 0;
+            }
+        }catch (Exception e) {
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return check;
+    }
+    
+    public boolean updateMentorEvent (String mentorEventID, String mentorEventStatus) throws ClassNotFoundException, SQLException{
+        boolean check=false;
+        Connection con = null;
+        PreparedStatement stm = null;
+        try{
+            con=DBUtil.getConnection();
+            if(con !=null){
+                String sql = "UPDATE tblMentorEvent SET mentorEventStatus=? WHERE mentorEventID=?";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, mentorEventStatus);
+                stm.setString(2, mentorEventID);
+                check = stm.executeUpdate() > 0;
+            }
+        }catch (Exception e) {
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return check;
+    }
+    
+    public boolean removeMentorEvent (String mentorEventID) throws ClassNotFoundException, SQLException{
+        boolean check=false;
+        Connection con = null;
+        PreparedStatement stm = null;
+        try{
+            con=DBUtil.getConnection();
+            if(con !=null){
+                String sql = "UPDATE tblMentorEvent SET mentorEventStatus=? WHERE mentorEventID=?";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, "DACT");
+                stm.setString(2, mentorEventID);
+                check = stm.executeUpdate() > 0;
+            }
+        }catch (Exception e) {
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return check;
+    }
     
 }
--- web/addMentor.jsp
+++ web/addMentor.jsp
@@ -4,6 +4,7 @@
     Author     : Minh Khoa
 --%>
 
+<%@page import="com.group5.memtorEvent.MentorEventDTO"%>
 <%@page import="java.util.List"%>
 <%@page import="com.group5.users.UserDTO"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
@@ -11,16 +12,86 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Add Mentor Page</title>
     </head>
     <body>
-        <% List<UserDTO> mentorList = (List<UserDTO>) request.getAttribute("LIST_MENTOR");%>
-                                    
+        <% List<UserDTO> mentorList = (List<UserDTO>) request.getAttribute("LIST_MENTOR");
+            List<MentorEventDTO> mentorsThisEventList = (List<MentorEventDTO>) request.getAttribute("LIST_MENTOR_THIS_EVENT");
+        %>
+        <%
+            String eventID = request.getParameter("eventID");            
+            String eventName = request.getParameter("eventName");
+//            if (eventName.equals("")) {
+//                    eventID = (String) request.getAttribute("eventName");
+//            }
+        %>                            
+
+        <h1>Mentors in event <%=eventName%></h1>
+        <%  if (mentorsThisEventList != null) {     %>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Mentor Name</th>
+                </tr>
+            </thead>
+            <%
+                int count = 0;
+                for (MentorEventDTO mentorThisEvent : mentorsThisEventList) {
+                    count++;
+            %>
+            <tbody>
+                <tr>
+                    <td><%=count%></td>
+                    <td><%=mentorThisEvent.getMemtorName()%></td>
+                </tr>
+
+            </tbody>
+            <%      }
+            }
+        %>
+        </table>
         
+
+
+        <h1>Adding Mentor for event <%=eventName%></h1>
+
         <form action="MainController">
+            <select name="mentorID">
+                <%
+                    if (mentorList != null) {
+                        for (UserDTO mentor : mentorList) {
+                %>
+
+                <option value="<%=mentor.getId()%>"> <%=mentor.getName()%> </option>
+
+                <%}
+                    }
+                %>
+            </select>
+            <input type="hidden" name="eventID" value="<%= eventID%>"/>
+            <input type="hidden" name="eventName" value="<%= eventName%>"/>
+            <input type="submit" name="action" value="ConfirmAddMentor"/>
+        </form>
             
-            <input type="hidden" name="eventID" value="<%= eventID%>"/>                            
-            <input type="submit" name="action" value="AddMentor"/>
+        <h1>Removing Mentor for event <%=eventName%></h1>
+
+        <form action="MainController">
+            <select name="mentorID">
+                <%
+                    if (mentorList != null) {
+                        for (MentorEventDTO mentorThisEvent : mentorsThisEventList) {
+                %>
+
+                <option value="<%=mentorThisEvent.getMentorID()%>"> <%=mentorThisEvent.getMemtorName()%> </option>
+
+                <%}
+                    }
+                %>
+            </select>
+            <input type="hidden" name="eventID" value="<%= eventID%>"/>
+            <input type="hidden" name="eventName" value="<%= eventName%>"/>
+            <input type="submit" name="action" value="ConfirmRemoveMentor"/>
         </form>
     </body>
 </html>
